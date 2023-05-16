885c696024ad9a64736ff9e7fd9984165dfa3012
GittensJrW <GittensJrW@169422FNMLAPTOP>
2023-05-16 14:59:43 -0400

    Add project files.

---
+++ NYCEmpower2_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NYCEmpower2_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("NYCEmpower2_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("B9E3EA90-A6DA-4232-87E2-A0A23151AE89")]
+[assembly: AssemblyVersion("1.0.0.0")]
